4562f12e3e608d3f9d7e809f9625f6cc93346c8d
Frugone <frugone@gmail.com>
2022-05-25 01:48:48 +0200

    add config file to provider

--- src/ClearLogsServiceProvider.php
+++ src/ClearLogsServiceProvider.php
@@ -9,5 +9,16 @@ class ClearLogsServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->commands([ClearLogs::class]);
+        $this->loadConfig();
+    }
+
+    private function loadConfig(): void
+    {
+        $this->publishes(
+            [
+                __DIR__ . '/../config/clearlogs.php' => config_path('clearlogs.php'),
+            ],
+            'clear-logs-config'
+        );
     }
 }
