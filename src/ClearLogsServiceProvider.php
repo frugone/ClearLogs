7004fe7f4ad00b1a82a8c1e27b05b04668f4acfc
Frugone <frugone@gmail.com>
2022-06-03 15:51:17 +0200

    fix error when publishing config file

--- src/ClearLogsServiceProvider.php
+++ src/ClearLogsServiceProvider.php
@@ -7,19 +7,24 @@ use Frugone\ClearLogs\Console\Commands\ClearLogs;
 
 class ClearLogsServiceProvider extends ServiceProvider
 {
+
+    public function boot()
+    {
+         $this->publishes(
+            [
+                __DIR__ . '/../config/clearlogs.php' => config_path('clearlogs.php'),
+            ]
+        );
+    }
+
     public function register(): void
     {
         $this->commands([ClearLogs::class]);
-        $this->loadConfig();
-    }
 
-    private function loadConfig(): void
-    {
-        $this->publishes(
-            [
-                __DIR__ . '/../config/clearlogs.php' => config_path('clearlogs.php'),
-            ],
-            'clear-logs-config'
+        $this->mergeConfigFrom(
+                __DIR__.'/../config/clearlogs.php', 'clearlogs'
         );
     }
+
+
 }
